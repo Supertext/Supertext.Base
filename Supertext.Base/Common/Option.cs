dd3cd8a3024b14128a3118f0583db660ad9594f1
wedge76 <hperegz@gmail.com>
2018-10-29 21:42:21 +0100

    Base: Option class with tests added

---
+++ Supertext.Base/Common/Option.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Supertext.Base.Common
+{
+    public struct Option<T>
+    {
+        private T _value;
+
+        private Option(T value)
+        {            
+            _value = value;
+            IsSome = true;
+        }
+
+        public bool IsSome { get; }
+
+        public bool IsNone => !IsSome;
+
+        public T Value
+        {
+            get
+            {
+                if (!IsSome) throw new NullReferenceException();
+                return _value;
+            }
+        }
+
+        public static Option<T> None()
+        {
+            return new Option<T>();
+        }
+
+        public static Option<T> Some(T value)
+        {
+            Validate.NotNull(value, nameof(value));
+            return new Option<T>(value);
+        }
+    }
+}
