7dffa364a55c671eb4fc8fdec8282710ede4216a
peter <peter@supertext.ch>
2018-10-29 10:45:43 +0100

    Base: - EnumerableExtensions and Validate class added

---
+++ Supertext.Base/Common/Validate.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supertext.Base.Common
+{
+    public static class Validate
+    {
+        private const string EmptyEnumerableMessage = "enumerable is empty";
+        private const string BlankStringMessage = "string is blank";
+        private const string EmptyStringMessage = "string is empty";
+
+        /// <summary>
+        /// throws ArgumentException if expression is false.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <exception cref="ArgumentException">thrown if expression is false</exception>
+        public static void IsTrue(bool expression)
+        {
+            if (!expression)
+            {
+                throw new ArgumentException("expression is false");
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException(message) if expression is false.
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="message">for exception</param>
+        /// <exception cref="ArgumentException">thrown if expression is false</exception>
+        public static void IsTrue(bool expression, string message)
+        {
+            if (!expression)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException if given enumerable is empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <exception cref="ArgumentException">thrown if enumerable is empty</exception>
+        public static void NotEmpty<T>(IEnumerable<T> enumerable)
+        {
+            if (enumerable == null || !enumerable.Any())
+            {
+                throw new ArgumentException(EmptyEnumerableMessage);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException(message) if given enumerable is empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <param name="parameterName"></param>
+        /// <exception cref="ArgumentException">thrown if enumerable is empty</exception>
+        public static void NotEmpty<T>(ICollection<T> enumerable, string parameterName)
+        {
+            if (enumerable == null || !enumerable.Any())
+            {
+                throw new ArgumentException(EmptyEnumerableMessage, parameterName);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException, if given string is blank
+        /// </summary>
+        /// <param name="str"></param>
+        /// <exception cref="ArgumentException">thrown, if string is blank</exception>
+        public static void NotBlank(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(BlankStringMessage);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException(message), if given string is blank
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="parameterName"></param>
+        /// <exception cref="ArgumentException">thrown, if string is blank</exception>
+        public static void NotBlank(string str, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(BlankStringMessage, parameterName);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException, if string is empty
+        /// </summary>
+        /// <param name="str"></param>
+        /// <exception cref="ArgumentException">thrown, if string is empty</exception>
+        public static void NotEmpty(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException(EmptyStringMessage);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException(message), if string is empty
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="paramterName"></param>
+        /// <exception cref="ArgumentException">thrown, if string is empty</exception>
+        public static void NotEmpty(string str, string paramterName)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException(EmptyStringMessage, paramterName);
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException, if object is null
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <exception cref="ArgumentException">thrown, if object is null</exception>
+        public static void NotNull(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        /// <summary>
+        /// throws ArgumentException(message), if object is null
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="parameterName"></param>
+        /// <exception cref="ArgumentException">thrown, if object is null</exception>
+        public static void NotNull(object obj, string parameterName)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+    }
+}
