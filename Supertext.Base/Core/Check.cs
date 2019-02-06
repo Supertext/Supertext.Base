ab0c3d2eb13814ca38a2f440b59efc64d5508cbc
Peter Höber <hperegz@gmail.com>
2019-02-06 21:29:48 +0100

    Base: Core DDD classes added

---
+++ Supertext.Base/Core/Check.cs
@@ -0,0 +1,10 @@
+﻿namespace Supertext.Base.Core
+{
+    public static class Check
+    {
+        public static CheckConstraint That(bool assertion)
+        {
+            return new CheckConstraint(assertion);
+        }
+    }
+}
