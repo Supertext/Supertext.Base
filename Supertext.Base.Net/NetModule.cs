b3fd261351b4ebee97abfdf520e273c6dcbd48e3
Peter Höber <peter@supertext.ch>
2019-07-02 08:22:50 +0200

    Net: New project Supertext.Base.Net with IHttpClientFactory added

---
+++ Supertext.Base.Net/NetModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Supertext.Base.Net.Http;
+
+namespace Supertext.Base.Net
+{
+    public class NetModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<HttpClientFactory>().As<IHttpClientFactory>();
+        }
+    }
+}
