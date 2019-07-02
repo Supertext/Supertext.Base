64658abdd8d7c6e5de76d7f3fb0597187e3e5a61
Peter Höber <peter@supertext.ch>
2019-07-02 08:54:50 +0200

    Conversion: New project Supertext.Base.Conversion with IJsonConverter added

---
+++ Supertext.Base.Conversion/Json/IJsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Supertext.Base.Conversion.Json
+{
+    public interface IJsonConverter
+    {
+        TResult Deserialize<TResult>(string content);
+
+        object DeserializeObject(string content);
+
+        string Serialize<TObject>(TObject jsonObject);
+
+        string GetTokenValue(string json, string tokenName);
+
+        string ReplaceTokenValue(string parent, string tokenName, Func<string, string> mapValueCallback);
+    }
+}
