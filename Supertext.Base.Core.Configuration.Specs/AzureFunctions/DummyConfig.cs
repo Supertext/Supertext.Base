9e65f684dabdca462d37479490bf2332c987b950
Andrew Jameson <jameson@supertext.ch>
2019-09-23 11:36:30 +0100

    Added settings retrieval for .NET Core-based Azure Functions. #3196

---
+++ Supertext.Base.Core.Configuration.Specs/AzureFunctions/DummyConfig.cs
@@ -0,0 +1,12 @@
+﻿using Supertext.Base.Configuration;
+
+namespace Supertext.Base.Core.Configuration.Specs.AzureFunctions
+{
+    public class DummyConfig : IConfiguration
+    {
+        public string Username { get; set; }
+
+        [SettingsKey("groupshare.username")]
+        public string GroupshareUserName { get; set; }
+    }
+}
