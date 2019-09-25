9f149f10a7be653f447391658c5fab952eb78256
Peter Höber <peter@supertext.ch>
2019-09-25 11:11:30 +0200

    Logging: LoggingExtensions moved to Supertext.Base.Core.Configuration. Library Supertext.Base.Extensions.Logging removed

--- Supertext.Base.Core.Configuration/Logging/LoggingExtensions.cs
+++ Supertext.Base.Core.Configuration/Logging/LoggingExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Supertext.Base.Extensions.Logging
+namespace Supertext.Base.Core.Configuration.Logging
 {
     public static class LoggingExtensions
     {
