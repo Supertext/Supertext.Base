68c734053d02a7859bc86dc338a0c6d6bc66ccdc
Peter Höber <peter@supertext.ch>
2019-12-30 12:12:21 +0100

    Security: New project added with KeyVaultCertificateService

---
+++ Supertext.Base.Security/SecurityModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Supertext.Base.Security.Cryptography;
+
+namespace Supertext.Base.Security
+{
+    public class SecurityModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<KeyVaultCertificateService>().As<ICertificateService>();
+        }
+    }
+}
