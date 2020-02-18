bd03aa35dc917a918df85ff212375fb115a75aab
Peter Höber <peter@supertext.ch>
2020-02-18 09:17:58 +0100

    Identity: Description for extension method RegisterClaimAuthorizationComponents() added

--- Supertext.Base.Identity/Authorization/AuthorizationExtensions.cs
+++ Supertext.Base.Identity/Authorization/AuthorizationExtensions.cs
@@ -5,6 +5,13 @@ namespace Supertext.Base.Identity.Authorization
 {
     public static class AuthorizationExtensions
     {
+        /// <summary>
+        /// Extension method for API authorization configuration.
+        /// Make sure, services.AddAuthorization() is being added.
+        /// It registers components as AuthorizationPolicyProvider and ClaimAuthorizeHandler.
+        /// Those components are helpful for API authorization with decorated controllers by the AuthorizeByClaimAttribute.
+        /// </summary>
+        /// <param name="services"></param>
         public static void RegisterClaimAuthorizationComponents(this IServiceCollection services)
         {
             services.AddSingleton<IAuthorizationPolicyProvider, AuthorizationPolicyProvider>();
