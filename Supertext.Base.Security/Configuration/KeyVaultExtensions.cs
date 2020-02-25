605b79aabadace9e1b2d9268f1d5edd09a5b6661
Peter Höber <peter@supertext.ch>
2020-02-25 15:48:34 +0100

    Security: KeyVaultExtensions.ConfigureKeyVaultAppConfiguration added to extend configuration with values from key vault.

---
+++ Supertext.Base.Security/Configuration/KeyVaultExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Hosting;
+
+namespace Supertext.Base.Security.Configuration
+{
+    public static class KeyVaultExtensions
+    {
+        /// <summary>
+        /// Adds Azure key vault to the configuration for environments Staging and Production.
+        /// </summary>
+        /// <param name="hostBuilder"></param>
+        /// <returns></returns>
+        public static IHostBuilder ConfigureKeyVaultAppConfiguration(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureAppConfiguration((context, config) =>
+                                                         {
+                                                             if (context.HostingEnvironment.IsStaging() || context.HostingEnvironment.IsProduction())
+                                                             {
+                                                                 var builtConfig = config.Build();
+                                                                 var vaultConfigSection = builtConfig.GetSection("KeyVault");
+                                                                 var vaultUrl = $"https://{vaultConfigSection["KeyVaultName"]}.vault.azure.net/";
+                                                                 config.AddAzureKeyVault(vaultUrl,
+                                                                                         vaultConfigSection["AzureADApplicationId"],
+                                                                                         vaultConfigSection["ClientSecret"],
+                                                                                         new DefaultKeyVaultSecretManager());
+                                                             }
+                                                         });
+        }
+    }
+}
