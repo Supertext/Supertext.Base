de96753a5b4357080126dc932aaced4ccd7e2125
Peter Höber <peter@supertext.ch>
2020-02-25 15:13:05 +0100

    Localization: Localization extension from Supertext.Base is used #4120

--- Supertext.Base.Core.Configuration/Localization/LocalizationExtensions.cs
+++ Supertext.Base.Core.Configuration/Localization/LocalizationExtensions.cs
@@ -8,10 +8,14 @@ namespace Supertext.Base.Core.Configuration.Localization
 {
     public static class LocalizationExtensions
     {
-        public static void AddSupertextLocalization(this IServiceCollection services, ICollection<CultureInfo> additionalCultures)
+        public static void AddSupertextLocalization(this IServiceCollection services, ICollection<CultureInfo> additionalCultures = null)
         {
             var cultures = new List<CultureInfo>(DefaultCultures.SupertextDefaultCultures);
-            cultures.AddRange(additionalCultures);
+            if (additionalCultures != null)
+            {
+                cultures.AddRange(additionalCultures);
+            }
+
             services.Configure<RequestLocalizationOptions>(options =>
                                                            {
                                                                options.DefaultRequestCulture = new RequestCulture(DefaultCultures.DefaultCultureInfo.Name);
