f5c6ce5850bc3b77e2a8bdba02dfdc87eb6d404d
Yasaman Jafari <yasaman@supertext.ch>
2020-03-05 11:29:16 +0100

    Emailing: test the local mail saving #4183

--- Supertext.Base.Core.Specs/MailServiceTest.cs
+++ Supertext.Base.Core.Specs/MailServiceTest.cs
@@ -1,8 +1,13 @@
+using System;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using Aspose.Email;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Supertext.Base.Net.Mail;
 using FakeItEasy;
+using FluentAssertions;
 
 namespace Supertext.Base.Net.Specs
 {
@@ -22,7 +27,7 @@ namespace Supertext.Base.Net.Specs
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void EmailService_SendGridEnabled_EmailShouldBeSentBySendGrid()
         {
             var mailServiceBoxedObject = RuntimeHelpers.GetObjectValue(_testee);
             _testee.GetType().GetProperty("Message")?.SetValue(mailServiceBoxedObject, "I'm testing the email service.");
@@ -38,6 +43,39 @@ namespace Supertext.Base.Net.Specs
             _config = (MailServiceConfig)configBoxedObject;
 
             _testee.Send("", "", "", "");
+        }
+
+        [TestMethod]
+        public void EmailService_SendGridDisabled_EmailShouldBeStoredLocally()
+        {
+            var dir = @"C:\Tmp\Emails";
+            var mailServiceBoxedObject = RuntimeHelpers.GetObjectValue(_testee);
+            _testee.GetType().GetProperty("Message")?.SetValue(mailServiceBoxedObject, "I'm testing the email service.");
+            _testee.GetType().GetProperty("Subject")?.SetValue(mailServiceBoxedObject, "Test");
+
+            var configBoxedObject = RuntimeHelpers.GetObjectValue(_config);
+            _config.GetType().GetProperty("SendGridEnabled")?.SetValue(configBoxedObject, false);
+            _config.GetType().GetProperty("LocalEmailDirectory")?.SetValue(configBoxedObject, dir);
+
+            _testee = (MailService)mailServiceBoxedObject;
+            _config = (MailServiceConfig)configBoxedObject;
+
+            _testee.Send("Tester", "tester@mail.com", "Verifier", "verifier@mail.com");
+
+            var directory = new DirectoryInfo(dir);
+
+            var myFile = directory.GetFiles()
+                                  .OrderByDescending(f => f.LastWriteTime)
+                                  .First();
+
+            var mailMessage = MailMessage.Load(myFile.FullName);
+            var body = mailMessage.Body;
+            var subject = mailMessage.Subject;
+            var from = mailMessage.From.Address;
+
+            body.Should().Equals(_testee.Message);
+            subject.Should().Equals(_testee.Subject);
+            from.Should().Equals("tester@mail.com");
         }
     }
 }
