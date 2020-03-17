fef03f0254619460bc60c8551701b65eb1593271
Andrija Karadžić <andrija@supertext.com>
2020-03-17 13:36:38 +0100

    Nwebsec api optimization

--- Supertext.Base.Security/NWebSec/Api/NWebSecApiExtensions.cs
+++ Supertext.Base.Security/NWebSec/Api/NWebSecApiExtensions.cs
@@ -5,15 +5,11 @@ namespace Supertext.Base.Security.NWebSec.Api
 {
     public static class Extensions
     {
-        public static void NwebSecApiPreStaticFilesSetup(this IApplicationBuilder app)
+        public static void NwebSecApiSetup(this IApplicationBuilder app)
         {
             var config = app.ApplicationServices.GetService<NWebSecConfig>();
             app.UseHsts(hsts => hsts.MaxAge(config.StrictTransportSecurityHeaderMaxAge));
             app.UseReferrerPolicy(opts => opts.NoReferrer());
-        }
-
-        public static void NwebSecApiPostStaticFilesSetup(this IApplicationBuilder app)
-        {
             app.UseXfo(options => options.SameOrigin());
         }
     }
