960a3339742a4da5dc3fd99aeed50b4385cd72f8
Peter Höber <peter@supertext.ch>
2020-04-10 16:46:25 +0200

    Test.Mvc: New .netcore project with web components for integration tests.

--- Supertext.Base.Test.Mvc/IntegrationTestWebApplicationFactory.cs
+++ Supertext.Base.Test.Mvc/IntegrationTestWebApplicationFactory.cs
@@ -14,11 +14,15 @@ using Supertext.Base.Test.Utils.Logging;
 
 namespace Supertext.Base.Test.Mvc
 {
+    public class IntegrationTestWebApplicationFactory : WebApplicationFactory<IntegrationTestWebApplicationFactory>
+    {
+    }
+
     /// <summary>
     /// Factory for bootstrapping an application in memory for functional end to end tests.
     /// </summary>
     /// <typeparam name="TStartup"></typeparam>
-    public class IntegrationTestWebApplicationFactory<TStartup> : WebApplicationFactory<IntegrationTestWebApplicationFactory<TStartup>> where TStartup : class, new()
+    public class IntegrationTestWebApplicationFactory<TStartup> : IntegrationTestWebApplicationFactory where TStartup : class, new()
     {
         private readonly string _url;
         private readonly IList<Claim> _userClaims;
