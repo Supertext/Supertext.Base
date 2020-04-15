6d15f6f7c00b130dd45ecf5bed3261029b612676
Peter Höber <peter@supertext.ch>
2020-04-15 12:06:46 +0200

    EntityFrameworkCore: New library with IHost extension for migrating the database.

---
+++ Supertext.Base.EntityFrameworkCore/Migration/HostExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Supertext.Base.EntityFrameworkCore.Migration
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TDbContext>(this IHost host) where TDbContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                using (var context = services.GetRequiredService<TDbContext>())
+                {
+                    try
+                    {
+                        context.Database.Migrate();
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = services.GetRequiredService<ILogger<TDbContext>>();
+                        logger.LogError(ex, "An error has occurred while migrating the database.");
+                        throw;
+                    }
+                }
+            }
+            return host;
+        }
+    }
+}
