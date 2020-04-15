239b322e55a060adbbe8d5d449ade090c8334440
Peter Höber <peter@supertext.ch>
2020-04-15 17:15:52 +0200

    EntityFrameworkCore: Extension for data seeding added

---
+++ Supertext.Base.EntityFrameworkCore/DataSeeding/ISeed.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Supertext.Base.EntityFrameworkCore.DataSeeding
+{
+    public interface ISeed
+    {
+        void Seed(ModelBuilder modelBuilder);
+    }
+}
---
+++ Supertext.Base.EntityFrameworkCore/DataSeeding/ModelBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Supertext.Base.EntityFrameworkCore.DataSeeding
+{
+    public static class ModelBuilderExtensions
+    {
+        /// <summary>
+        /// Configures data seeding
+        /// </summary>
+        /// <typeparam name="TSeed">Represents type of an assembly which implements ISeed. All types of that assembly which implement ISeed are being taken.</typeparam>
+        /// <param name="modelBuilder"></param>
+        public static void Seed<TSeed>(this ModelBuilder modelBuilder) where TSeed : ISeed
+        {
+            var seedingTypes = typeof(TSeed).Assembly.GetTypes().Where(type => type.GetInterfaces().Contains(typeof(ISeed)));
+            foreach (var seedType in seedingTypes)
+            {
+                var seed = Activator.CreateInstance(seedType) as ISeed;
+                seed?.Seed(modelBuilder);
+            }
+        }
+    }
+}
