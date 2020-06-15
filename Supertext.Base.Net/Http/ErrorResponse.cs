69f3d3fa496bc4c7639b3b4f5801750598e6d1c4
Peter Höber <peter@supertext.ch>
2020-06-15 15:40:56 +0200

    Hosting: Exception handling middleware added

---
+++ Supertext.Base.Net/Http/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Supertext.Base.Net.Http
+{
+    public class ErrorResponse
+    {
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.InternalServerError;
+
+        public string Message { get; set; } = "An unexpected error occurred.";
+    }
+}
---
+++ Supertext.Base.Net/Http/HttpException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace Supertext.Base.Net.Http
+{
+    /// <summary>
+    /// Can be used to throw exceptions in API controllers. It will be caught and handled by an exception middleware.
+    /// The middleware can be configured by using <see cref="Supertext.Base.Hosting.Extensions.ExceptionHandlingMiddlewareExtensions"/>
+    /// </summary>
+    public class HttpException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        public HttpException(HttpStatusCode statusCode, string message)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
