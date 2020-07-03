469d45b89a1db1bed860273390d2807f771cea6b
Vito Melchionna <vito@supertext.ch>
2020-07-03 09:44:46 +0200

    Security: Removed project that wasn't deleted properly.

--- Supertext.Base.Security.KeyVault/Keys/IAzureKeyReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography;
-
-namespace Supertext.Base.Security.KeyVault.Keys
-{
-    internal interface IAzureKeyReader
-    {
-        string ReadPrivateKey(RSA privateKey);
-    }
-}
