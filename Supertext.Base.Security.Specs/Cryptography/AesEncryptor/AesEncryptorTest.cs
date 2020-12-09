8a05a11273a25f8a2e1cc64edb4f08110fba775e
Yannick Ruck <yannick@supertext.ch>
2020-12-09 11:43:47 +0100

    added AesEncryptorTest #5907

---
+++ Supertext.Base.Security.Specs/Cryptography/AesEncryptor/AesEncryptorTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Supertext.Base.Security.Cryptography.AesEncryptor;
+
+namespace MonoCms.Client.Tests.Common
+{
+    [TestClass]
+    public class AesEncryptorTest
+    {
+        private AesEncryptor _testee;
+        private string _referenceId;
+        private EncryptionConfig _config;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _config = new EncryptionConfig() { Key = "MyEncryptionText", Iv = "IVsAreVeryNiceOk" };
+            _testee = new AesEncryptor(_config);
+            _referenceId = "test";
+        }
+
+        [TestMethod]
+        public void Encrypt_ReferenceIsGivenAndIsEncrypted_WhenTheResultIsDecryptedItsTheSameAsTheOriginalMessage()
+        {
+            var encryptedReference = _testee.Encrypt(_referenceId);
+
+            var decryptedReference = _testee.Decrypt(encryptedReference);
+            decryptedReference.Should().Be(_referenceId);
+        }
+
+        [TestMethod]
+        public void Decrypt_IVisDifferentThanTheOneUsedForEncryption_ReturnsEmptyString()
+        {
+            var encryptedReference = _testee.Encrypt(_referenceId);
+            _config = new EncryptionConfig() { Key = "MyEncryptionText", Iv = "ThisIsDifferent!" };
+            _testee = new AesEncryptor(_config);
+
+            var decryptedReference = _testee.Decrypt(encryptedReference);
+
+            decryptedReference.Should().Be("");
+        }
+
+        [TestMethod]
+        public void Decrypt_KeyisDifferentThanTheOneUsedForEncryption_ReturnsEmptyString()
+        {
+            var encryptedReference = _testee.Encrypt(_referenceId);
+            _config = new EncryptionConfig() { Key = "NewEncryptionKey", Iv = "IVsAreVeryNiceOk" };
+            _testee = new AesEncryptor(_config);
+
+            var decryptedReference = _testee.Decrypt(encryptedReference);
+
+            decryptedReference.Should().Be(String.Empty);
+        }
+    }
+}
