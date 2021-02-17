10fdd0cdba94714b0128f0817e7760f1c27dae97
Vito Melchionna <vito@supertext.ch>
2021-02-17 10:07:48 +0100

    Cryptography: Added missing IConfiguration interface

--- Supertext.Base.Security.Cryptography/Hashing/HashingConfig.cs
+++ Supertext.Base.Security.Cryptography/Hashing/HashingConfig.cs
@@ -3,11 +3,10 @@
 namespace Supertext.Base.Security.Cryptography.Hashing
 {
     [ConfigSection("EncryptionSettings")]
-    public class HashingConfig
+    public class HashingConfig : IConfiguration
     {
         [KeyVaultSecret("Hashing-Pepper-For-Tokens")]
         public string HashingPepperForToken { get; set; }
-
 
         [KeyVaultSecret("Hashing-Pepper-For-Passwords")]
         public string HashingPepperForPassword { get; set; }
