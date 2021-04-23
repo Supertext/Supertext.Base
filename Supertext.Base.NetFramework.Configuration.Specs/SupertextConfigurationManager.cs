d40854ed197c7bcb391a1d618ad7b708e2499b08
Vito Melchionna <vito@supertext.ch>
2021-04-23 15:29:02 +0200

    Bug6628: Added integration tests for SupertextConfigurationManager

---
+++ Supertext.Base.NetFramework.Configuration.Specs/SupertextConfigurationManager.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Supertext.Base.NetFramework.Configuration.Specs
+{
+    [TestClass]
+    public class SupertextConfigurationManager
+    {
+        [TestMethod]
+        public void AppSettings_AppSettingsAvailable_GetsNameValueCollectionWithAllRegisteredSettings()
+        {
+            var appSettings = Configuration.SupertextConfigurationManager.AppSettings;
+            appSettings["AnotherInt"].Should().Be("9712");
+            appSettings["AnotherString"].Should().Be("another value");
+            appSettings["Super-Secret"].Should().Be("very secret value");
+        }
+
+        [TestMethod]
+        public void AppSettings_UnavailableAppSetting_ReturnsNull()
+        {
+            var appSettings = Configuration.SupertextConfigurationManager.AppSettings;
+            appSettings["UnavailableKey"].Should().Be(null);
+        }
+
+        [TestMethod]
+        public void GetAppSetting_ExistentAppSetting_ReturnsValueAsString()
+        {
+            var appSetting = Configuration.SupertextConfigurationManager.GetAppSetting("AnotherInt");
+            appSetting.Should().Be("9712");
+        }
+
+        [TestMethod]
+        public void GetAppSetting_ExistentAppSettingInFallBackConfigurationManager_ReturnsValueAsString()
+        {
+            var appSetting = Configuration.SupertextConfigurationManager.GetAppSetting("SomeInt");
+            appSetting.Should().Be("4711");
+        }
+
+        [TestMethod]
+        public void GetAppSetting_UnavailableAppSetting_ReturnsNull()
+        {
+            var appSetting = Configuration.SupertextConfigurationManager.GetAppSetting("UnavailableKey");
+            appSetting.Should().Be(null);
+        }
+    }
+}
