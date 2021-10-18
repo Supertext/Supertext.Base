33bf4a95bf9ee2be3f0df0190a7afe9924401ec6
Peter Höber <peter@supertext.ch>
2021-10-18 17:26:17 +0200

    Configuration: Handling of nested configs with KeyVaultSecretAttribute added

---
+++ Supertext.Base.Core.Configuration.Specs/Extensions/Hierarchy2.cs
@@ -0,0 +1,12 @@
+﻿using Supertext.Base.Configuration;
+
+namespace Supertext.Base.Core.Configuration.Specs.Extensions
+{
+    public class Hierarchy2
+    {
+        [KeyVaultSecret("PersonSecretName")]
+        public string Secret { get; set; }
+
+        public Hierarchy3 Hierarchy3 { get; set; }
+    }
+}
