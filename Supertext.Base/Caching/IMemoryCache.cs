6ccf011cd647039f2d305873a5f15037b47c555b
Peter Höber <peter@supertext.ch>
2022-01-04 12:20:16 +0100

    MemoryCache: Async factory method added.

--- Supertext.Base/Caching/IMemoryCache.cs
+++ Supertext.Base/Caching/IMemoryCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Supertext.Base.Common;
 
 namespace Supertext.Base.Caching
@@ -8,6 +9,7 @@ namespace Supertext.Base.Caching
         void Add(string key, T item);
         Option<T> Get(string key);
         T GetOrCreateAndGet(string key, Func<string, T> factoryMethod);
+        Task<T> GetOrCreateAndGetAsync(string key, Func<string, Task<T>> factoryMethod);
         void Remove(string key);
     }
 }
