de8a3d9f11a21bfbad3b5d970ba7ff36168aeaca
Peter Höber <peter@supertext.ch>
2022-05-13 17:33:36 +0200

    Upgrade to .net6.0

--- Supertext.Base.Security.Cryptography/AesEncryption/AesEncryptor.cs
+++ Supertext.Base.Security.Cryptography/AesEncryption/AesEncryptor.cs
@@ -6,25 +6,23 @@ namespace Supertext.Base.Security.Cryptography.AesEncryption
 {
     internal class AesEncryptor : IAesEncryptor
     {
-        private readonly AesCryptoServiceProvider _cryptoServiceProvider;
+        private readonly Aes _aes;
         private const int InputOffset = 0;
 
         public AesEncryptor(EncryptionConfig config)
         {
-            _cryptoServiceProvider = new AesCryptoServiceProvider
-                                     {
-                                         BlockSize = 128,
-                                         KeySize = 128,
-                                         Mode = CipherMode.CBC,
-                                         Padding = PaddingMode.PKCS7,
-                                         Key = Encoding.UTF8.GetBytes(config.Key),
-                                         IV = Encoding.UTF8.GetBytes(config.Iv)
-                                     };
+            _aes = Aes.Create();
+            _aes.BlockSize = 128;
+            _aes.KeySize = 128;
+            _aes.Mode = CipherMode.CBC;
+            _aes.Padding = PaddingMode.PKCS7;
+            _aes.Key = Encoding.UTF8.GetBytes(config.Key);
+            _aes.IV = Encoding.UTF8.GetBytes(config.Iv);
         }
 
         public string Encrypt(string text)
         {
-            var transform = _cryptoServiceProvider.CreateEncryptor();
+            var transform = _aes.CreateEncryptor();
             var encryptedBytes = transform.TransformFinalBlock(Encoding.UTF8.GetBytes(text), InputOffset, text.Length);
 
             return Convert.ToBase64String(encryptedBytes);
@@ -32,7 +30,7 @@ namespace Supertext.Base.Security.Cryptography.AesEncryption
 
         public string Decrypt(string encryptedText)
         {
-            var transform = _cryptoServiceProvider.CreateDecryptor();
+            var transform = _aes.CreateDecryptor();
             var encryptedBytes = Convert.FromBase64String(encryptedText);
             var decryptedBytes = Convert.FromBase64String(String.Empty);
             try
@@ -49,7 +47,7 @@ namespace Supertext.Base.Security.Cryptography.AesEncryption
 
         public void Dispose()
         {
-            _cryptoServiceProvider?.Dispose();
+            _aes?.Dispose();
         }
     }
 }
