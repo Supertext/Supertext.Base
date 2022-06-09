aa76357fe02b97b3c4e3c7d406d9a63c8121e4a4
ivan stankovic <ivan@supertext.com>
2022-06-09 13:53:51 +0200

    IdentityServier4: Added IdentityServer4

---
+++ Supertext.Base.IdentityServer4.AccessTokenValidation/IdentityServerAuthenticationDefaults.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Supertext.Base.IdentityServer4.AccessTokenValidation
+{
+    /// <summary>
+    /// Constants for IdentityServer authentication.
+    /// </summary>
+    public class IdentityServerAuthenticationDefaults
+    {
+        /// <summary>
+        /// The authentication scheme
+        /// </summary>
+        public const string AuthenticationScheme = "Bearer";
+
+        /// <summary>
+        /// Value of the JWT typ header (IdentityServer4 v3+ sets this by default)
+        /// </summary>
+        public const string JwtAccessTokenTyp = "at+jwt";
+
+        internal const string IntrospectionAuthenticationScheme = "IdentityServerAuthenticationIntrospection";
+        internal const string JwtAuthenticationScheme = "IdentityServerAuthenticationJwt";
+        internal const string TokenItemsKey = "idsrv4:tokenvalidation:token";
+        internal const string EffectiveSchemeKey = "idsrv4:tokenvalidation:effective:";
+    }
+}
