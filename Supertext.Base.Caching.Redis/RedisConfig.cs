85504699a79f9f61edc0f54c6606b3826eaed853
Peter Höber <peter@supertext.ch>
2023-03-14 15:24:35 +0100

    Caching components with Redis added

---
+++ Supertext.Base.Caching.Redis/RedisConfig.cs
@@ -0,0 +1,19 @@
+﻿using Supertext.Base.Configuration;
+
+namespace Supertext.Base.Caching.Redis;
+
+[ConfigSection("Redis")]
+internal class RedisConfig : IConfiguration
+{
+    [KeyVaultSecret("redisConnection")]
+    public string ConnectionString { get; set; }
+
+    public int Timeout { get; set; }
+
+    public int ConnectRetry { get; set; }
+
+    [KeyVaultSecret("redisPassword")]
+    public string Password { get; set; }
+
+    public bool UseSsl { get; set; }
+}
