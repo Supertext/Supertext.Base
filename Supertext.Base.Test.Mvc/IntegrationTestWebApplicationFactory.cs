20e0ffa91f76ac8da11b4e8e697defaa5b5b4cc3
Peter Höber <peter@supertext.ch>
2023-07-06 14:01:38 +0200

    Test.Mvc: Serilog.Extensions.Hosting added

--- Supertext.Base.Test.Mvc/IntegrationTestWebApplicationFactory.cs
+++ Supertext.Base.Test.Mvc/IntegrationTestWebApplicationFactory.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using Supertext.Base.Extensions;
 using Supertext.Base.Test.Utils.Logging;
 
