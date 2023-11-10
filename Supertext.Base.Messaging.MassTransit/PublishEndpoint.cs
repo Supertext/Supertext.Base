63b8f7fc1f09953d84eb6a3b825bcc7822bee144
Peter Höber <peter@supertext.ch>
2023-11-10 08:30:59 +0100

    MassTransit: Constraints for message types removed

--- Supertext.Base.Messaging.MassTransit/PublishEndpoint.cs
+++ Supertext.Base.Messaging.MassTransit/PublishEndpoint.cs
@@ -17,13 +17,13 @@ namespace Supertext.Base.Messaging.MassTransit
             _logger = logger;
         }
 
-        public Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken) where TMessage : class, new()
+        public Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken)
         {
             _logger.LogDebug($"Sending message of type {typeof(TMessage).Name}.");
             return PublishAsync(message, Guid.NewGuid(), cancellationToken);
         }
 
-        public Task PublishAsync<TMessage>(TMessage message, Guid correlationId, CancellationToken cancellationToken) where TMessage : class, new()
+        public Task PublishAsync<TMessage>(TMessage message, Guid correlationId, CancellationToken cancellationToken)
         {
             _logger.LogDebug($"Sending message of type {typeof(TMessage).Name} with correlation ID {correlationId}.");
             return _publishEndpoint.Publish(message, context => context.CorrelationId = correlationId, cancellationToken);
