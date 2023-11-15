5ad03b75a27e538d607fe038dc81b02014805d5a
Peter Höber <peter@supertext.ch>
2023-11-15 09:00:03 +0100

    MassTransit: Settable concurrentMessageLimit for consumers

--- Supertext.Base.Hosting/MassTransit/BusRegistrationConfiguratorExtension.cs
+++ Supertext.Base.Hosting/MassTransit/BusRegistrationConfiguratorExtension.cs
@@ -4,10 +4,11 @@ namespace Supertext.Base.Hosting.MassTransit
 {
     public static class BusRegistrationConfiguratorExtension
     {
-        public static void RegisterMessage<TMessage>(this IBusRegistrationConfigurator configurator)
+        public static void RegisterMessage<TMessage>(this IBusRegistrationConfigurator configurator, int? concurrentMessageLimit = null)
             where TMessage : class
         {
-            configurator.AddConsumer<MessageConsumer<TMessage>>();
+            configurator.AddConsumer<MessageConsumer<TMessage>>(consumerConfigurator =>
+                                                                    consumerConfigurator.ConcurrentMessageLimit = concurrentMessageLimit);
         }
     }
 }
