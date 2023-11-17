e0bcae0a40b273694694267b214c07e9f7b9c23f
Heinrich Muralt <heinrich@supertext.ch>
2023-11-17 11:50:11 +0100

    Add custom exception handler and problem details writer for problem details error handling in Hosting

---
+++ Supertext.Base.Hosting/ErrorHandling/ProblemDetailsExceptionHandler.cs
@@ -0,0 +1,52 @@
+﻿#if NET8_0_OR_GREATER
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Supertext.Base.Exceptions;
+
+namespace Supertext.Base.Hosting.ErrorHandling
+{
+    public class ProblemDetailsExceptionHandler : IExceptionHandler
+    {
+        private readonly IProblemDetailsService _problemDetailsService;
+
+        public ProblemDetailsExceptionHandler(IProblemDetailsService problemDetailsService)
+        {
+            _problemDetailsService = problemDetailsService;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is ProblemDetailsException problemDetailsException)
+            {
+                var exceptionHandlerFeature = httpContext.Features.Get<IExceptionHandlerFeature>();
+                var statusCode = GetStatusCode(problemDetailsException, httpContext.Response.StatusCode == 0 ? StatusCodes.Status500InternalServerError : httpContext.Response.StatusCode);
+                httpContext.Response.StatusCode = statusCode;
+
+                return await _problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+                                                                  {
+                                                                      HttpContext = httpContext,
+                                                                      AdditionalMetadata = exceptionHandlerFeature.Endpoint?.Metadata,
+                                                                      ProblemDetails = { Status = statusCode },
+                                                                      Exception = exception,
+                                                                  });
+            }
+
+            return false;
+        }
+
+        private static int GetStatusCode(ProblemDetailsException exception, int fallbackStatusCode)
+        {
+            return exception switch
+            {
+                ForbiddenException _ => StatusCodes.Status403Forbidden,
+                UnauthorizedException _ => StatusCodes.Status401Unauthorized,
+                ConflictException _ => StatusCodes.Status409Conflict,
+                var _ => fallbackStatusCode
+            };
+        }
+    }
+}
+#endif
---
+++ Supertext.Base.Hosting/ErrorHandling/ProblemDetailsExceptionWriter.cs
@@ -0,0 +1,96 @@
+﻿#if NET8_0_OR_GREATER
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Supertext.Base.Exceptions;
+
+namespace Supertext.Base.Hosting.ErrorHandling
+{
+    public class ProblemDetailsExceptionWriter : IProblemDetailsWriter
+    {
+        private const string UnknownErrorOccurred = "Unknown error occurred";
+        private const string GeneralErrorKey = "general";
+
+        private static readonly MediaTypeCollection ProblemContentTypes = new()
+                                                                          {
+                                                                              "application/problem+json",
+                                                                              "application/problem+xml"
+                                                                          };
+
+        private readonly OutputFormatterSelector _formatterSelector;
+        private readonly IHttpResponseStreamWriterFactory _writerFactory;
+        private readonly ProblemDetailsFactory _problemDetailsFactory;
+
+
+
+        public ProblemDetailsExceptionWriter(OutputFormatterSelector formatterSelector,
+                                             IHttpResponseStreamWriterFactory writerFactory,
+                                             ProblemDetailsFactory problemDetailsFactory)
+        {
+            _formatterSelector = formatterSelector;
+            _writerFactory = writerFactory;
+            _problemDetailsFactory = problemDetailsFactory;
+        }
+
+        public bool CanWrite(ProblemDetailsContext context)
+        {
+            var exceptionHandlerFeature = context.HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return exceptionHandlerFeature?.Error is ProblemDetailsException;
+        }
+
+        public ValueTask WriteAsync(ProblemDetailsContext context)
+        {
+            var problemDetails = CreateNewProblemDetails(context);
+
+            ExtendProblemDetails(context, problemDetails);
+
+            var formatterContext = new OutputFormatterWriteContext(context.HttpContext,
+                                                                   _writerFactory.CreateWriter,
+                                                                   typeof(ProblemDetails),
+                                                                   problemDetails);
+
+            var formatter = _formatterSelector.SelectFormatter(formatterContext,
+                                                               Array.Empty<IOutputFormatter>(),
+                                                               ProblemContentTypes);
+
+            return formatter == null ? ValueTask.CompletedTask : new ValueTask(formatter.WriteAsync(formatterContext));
+        }
+
+        private ProblemDetails CreateNewProblemDetails(ProblemDetailsContext context)
+        {
+            var problemDetails = _problemDetailsFactory.CreateProblemDetails(context.HttpContext,
+                                                                             context.ProblemDetails.Status ?? context.HttpContext.Response.StatusCode,
+                                                                             context.ProblemDetails.Title,
+                                                                             context.ProblemDetails.Type,
+                                                                             context.ProblemDetails.Detail,
+                                                                             context.ProblemDetails.Instance);
+            foreach (var extension in context.ProblemDetails.Extensions)
+            {
+                problemDetails.Extensions[extension.Key] = extension.Value;
+            }
+
+            return problemDetails;
+        }
+
+        private static void ExtendProblemDetails(ProblemDetailsContext context, ProblemDetails problemDetails)
+        {
+            if (context.HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error is ProblemDetailsException exception)
+            {
+                problemDetails.Extensions.Add("errorKey", exception.ErrorKey);
+                problemDetails.Extensions.Add("message", exception.Message);
+                problemDetails.Extensions.Add("data", exception.Data);
+            }
+            else
+            {
+                problemDetails.Extensions.Add("errorKey", GeneralErrorKey);
+                problemDetails.Extensions.Add("message", UnknownErrorOccurred);
+            }
+        }
+    }
+}
+#endif
