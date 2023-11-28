4a9db361362f5c0719873154de948a746b66a45e
Peter Höber <peter@supertext.ch>
2023-11-28 10:42:10 +0100

    Hosting: Action renamed

--- Supertext.Base.Hosting/MassTransit/AutofacBuilderExtension.cs
+++ Supertext.Base.Hosting/MassTransit/AutofacBuilderExtension.cs
@@ -7,11 +7,18 @@ namespace Supertext.Base.Hosting.MassTransit;
 
 public static class AutofacBuilderExtension
 {
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="assembly"></param>
+    /// <param name="messageCorrelationIdProvider">Provides the correlation of the arrived message.</param>
+    /// <returns></returns>
     public static ContainerBuilder RegisterMessageConsumers(this ContainerBuilder builder,
                                                             Assembly assembly,
-                                                            Action<string> messageArrivedCallback = null)
+                                                            Action<string> messageCorrelationIdProvider = null)
     {
-        var enricher = new MessageConsumerEnricher(messageArrivedCallback);
+        var enricher = new MessageConsumerEnricher(messageCorrelationIdProvider);
         builder.RegisterInstance(enricher).AsSelf().SingleInstance();
         builder.RegisterModule<MessagingMassTransitModule>();
         builder.RegisterAssemblyTypes(assembly)
