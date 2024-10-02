23ec87e57078959afd2bdc6bde8a60ba7f75d6fc
Peter Höber <peter@supertext.ch>
2024-10-02 11:30:59 +0200

    EmailInfoTemplates with summary.

--- Supertext.Base/Net/Mail/EmailInfoTemplates.cs
+++ Supertext.Base/Net/Mail/EmailInfoTemplates.cs
@@ -2,7 +2,40 @@
 
 namespace Supertext.Base.Net.Mail
 {
-
+    /// <summary>
+    /// Represents an email template with dynamic data and recipient information.
+    /// </summary>
+    /// <typeparam name="TDynamicData">The type of the dynamic data which will be serialised into JSON. </typeparam>
+    /// <example>
+    /// Sample of TDynamicData:
+    /// <code>
+    /// public class ConfirmEmailData
+    /// {
+    ///     public AlertRow AlertRow { get; set; }
+    ///     public Message Message { get; set; }
+    ///     public Footer Footer { get; set; }
+    ///     public string Address { get; set; }
+    ///     public string Subject { get; set; }
+    /// }
+    ///
+    /// public class AlertRow
+    /// {
+    ///     public string Column1 { get; set; }
+    ///     public string Column2 { get; set; }
+    /// }
+    ///
+    /// public class Footer
+    /// {
+    ///     public string FooterMessage { get; set; }
+    /// }
+    ///
+    /// public class Message
+    /// {
+    ///     public string Salutation { get; set; }
+    ///     public string Message1 { get; set; }
+    /// }
+    /// </code>
+    /// </example>
     public class EmailInfoTemplates<TDynamicData>
     {
         public EmailInfoTemplates(string templateId,
