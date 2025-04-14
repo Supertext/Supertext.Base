f20dcbda9b1e57f3df8961389d5db52c33ac186a
Andrew Jameson <jameson@supertext.ch>
2025-04-14 11:17:54 +0200

    Updated tests for IMailService. #13017

--- Supertext.Base.Core.Specs/Mail/MailServiceTest.cs
+++ Supertext.Base.Core.Specs/Mail/MailServiceTest.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Aspose.Email;
-using FakeItEasy;
+using Autofac;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Supertext.Base.Modules;
 using Supertext.Base.Net.Mail;
 
 namespace Supertext.Base.Net.Specs.Mail
@@ -15,82 +10,72 @@ namespace Supertext.Base.Net.Specs.Mail
     [TestClass]
     public class MailServiceTest
     {
-        private MailService _testee;
-        private ILogger<IMailService> _logger;
-        private MailServiceConfig _config;
-        private EmailInfo _mail;
-        private string _dir;
-        private DirectoryInfo _testDir;
+        private IContainer _container;
 
-        [TestInitialize]
-        public void Setup()
+        [TestMethod]
+        public void InstantiateFromIoC_WhenSendGridIsDisabled_ReturnsFileSystemMailService()
         {
-            var path = Directory.GetCurrentDirectory();
-            _dir = String.Concat(path, "\\temp");
-            Directory.CreateDirectory(_dir);
-            _testDir = new DirectoryInfo(_dir);
-            _logger = A.Fake<ILogger<IMailService>>();
-            if (!_testDir.Exists)
+            SetupIocContainer<ConfigWithSendGridDisabled>();
+
+            var instance = _container.Resolve<IMailService>();
+
+            instance.Should().BeOfType<FileSystemMailService>();
+        }
+
+        [TestMethod]
+        public void InstantiateFromIoC_WhenSendGridIsEnabled_ReturnsSendGridMailService()
+        {
+            SetupIocContainer<ConfigWithSendGridEnabled>();
+
+            var instance = _container.Resolve<IMailService>();
+
+            instance.Should().BeOfType<SendGridMailService>();
+        }
+
+        private void SetupIocContainer<T>() where T : MailServiceConfig, new()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<BaseModule>();
+            builder.RegisterModule<NetModule>();
+
+            var loggerFactory = LoggerFactory.Create(config => config.AddConsole());
+
+            builder.RegisterInstance(loggerFactory)
+                   .As<ILoggerFactory>()
+                   .SingleInstance();
+
+            builder.RegisterGeneric(typeof(Logger<>))
+                   .As(typeof(ILogger<>))
+                   .SingleInstance();
+
+            builder.Register(_ => new T()).As<MailServiceConfig>();
+
+            _container = builder.Build();
+        }
+
+        private class ConfigWithSendGridDisabled : MailServiceConfig
+        {
+            public ConfigWithSendGridDisabled()
             {
-                _logger.LogError("Path for temporary local email storage is not correct.");
+                LocalEmailDirectory = @"C:\temp\emails";
+                SendGridEnabled = false;
+                SendGridHost = $"Test {nameof(ConfigWithSendGridDisabled)}.{nameof(SendGridHost)}";
+                SendGridPassword = $"Test {nameof(ConfigWithSendGridDisabled)}.{nameof(SendGridPassword)}";
+                SendGridUsername = $"Test {nameof(ConfigWithSendGridDisabled)}.{nameof(SendGridUsername)}";
             }
-
-            _config = new MailServiceConfig {LocalEmailDirectory = _dir};
-            _testee = new MailService(_logger, _config);
-
-            var to = new PersonInfo("Verifier", "verifier@mail.com");
-
-            var from = new PersonInfo("Tester", "tester@mail.com");
-
-            _mail = new EmailInfo("Test", "Testing the mail service.", from, to);
         }
 
-        [TestMethod]
-        public async Task EmailService_SendGridDisabled_EmailShouldBeStoredLocally()
+        private class ConfigWithSendGridEnabled : MailServiceConfig
         {
-            _config.SendGridEnabled = false;
-
-            await _testee.SendAsync(_mail);
-
-            var directory = new DirectoryInfo(_dir);
-
-            var myFile = directory.GetFiles()
-                                  .OrderByDescending(f => f.LastWriteTime)
-                                  .First();
-
-            var mailMessage = MailMessage.Load(myFile.FullName);
-
-            mailMessage.From.Address.Should().Be(_mail.From.Email);
-        }
-
-        [TestMethod]
-        public async Task EmailService_MultipleRecipients_Success()
-        {
-            var to1 = new PersonInfo("Verifier 1", "verifier1@mail.com");
-            var to2 = new PersonInfo("Verifier 2", "verifier2@mail.com");
-
-            var from = new PersonInfo("Tester", "tester@mail.com");
-
-            _mail = new EmailInfo("Test", "Testing the mail service.", from, new List<PersonInfo> { to1, to2 });
-
-            _config.SendGridEnabled = false;
-
-            await _testee.SendAsync(_mail);
-
-            var directory = new DirectoryInfo(_dir);
-
-            var myFile = directory.GetFiles()
-                                  .OrderByDescending(f => f.LastWriteTime)
-                                  .First();
-
-            var mailMessage = MailMessage.Load(myFile.FullName);
-            mailMessage.To.Select(t => t.Address).Should().BeEquivalentTo(new[] { to1.Email, to2.Email });
-        }
-
-        [TestCleanup]
-        public void TearDown()
-        {
-            Directory.Delete(_dir, true);
+            public ConfigWithSendGridEnabled()
+            {
+                LocalEmailDirectory = @"C:\temp\emails";
+                SendGridEnabled = true;
+                SendGridHost = $"Test {nameof(ConfigWithSendGridEnabled)}.{nameof(SendGridHost)}";
+                SendGridPassword = $"Test {nameof(ConfigWithSendGridEnabled)}.{nameof(SendGridPassword)}";
+                SendGridUsername = $"Test {nameof(ConfigWithSendGridEnabled)}.{nameof(SendGridUsername)}";
+            }
         }
     }
 }
